8335c32650be155c46ad0bcf4c8e222c246bb04c
ShriraamVelayutham <106417030+ShriraamVelayutham@users.noreply.github.com>
2023-07-14 19:17:48 -0700

    Add files via upload

---
+++ file_system_events_tracker.py
@@ -0,0 +1,25 @@
+import sys
+import time
+import random
+import os
+import shutil
+import observer
+import FileSystemEventHandler
+from watchdog.observers 
+from watchdog.events 
+
+from_dir = "<on_created(), on_modified, on_moved, on_deleted>"
+
+try:
+
+    while True:
+        time.sleep(2)
+        print("running...")
+except KeyboardInterrupt:
+    print("stopped!")
+    observer.stop()
+
+
+
+        
+
